b1e6c84ad6bfa769ec1cdcae4982f1019710075d
felixC89 <39625792+felixC89@users.noreply.github.com>
2022-02-11 11:50:37 -0600

    params rest y spreads

---
+++ src/parametrosRest.js
@@ -0,0 +1,5 @@
+const mostrarDatos = (...datos) => {
+  console.log(datos);
+};
+
+mostrarDatos("Antonio", 25, "antonio@correo.com");
---
+++ src/parametrosSpread.js
@@ -0,0 +1,7 @@
+const mostrarDatos = (...datos) => {
+  console.log(datos);
+};
+
+const arregloDatos = ["felix", 30, "correo@correo.com"];
+
+mostrarDatos(...arregloDatos);
